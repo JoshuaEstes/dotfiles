4f2975bc109d1ab1fd703ebcdc336f940e68078a
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-11-28 10:24:33 -0600

    bug fix

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for FILENAME in $( ls | grep -v README ); do ln -bis "$FILENAME" "$HOME/.$FILENAME"; done
+for FILENAME in $( ls | grep -v README | grep -v install.sh ); do ln -bis "$FILENAME" "$HOME/.$FILENAME"; done
