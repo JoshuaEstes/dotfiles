24110b19823946b5cf25e567667fc07a388de74d
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-11-29 10:54:35 -0600

    bug fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for FILENAME in $( ls | grep -v README | grep -v install.sh ); do echo "$FILENAME"; ln -bis "$FILENAME" "$HOME/.$FILENAME"; done
+for FILENAME in $( ls | grep -v README | grep -v install.sh ); do echo "$FILENAME"; ln -bis "$(pwd)/$FILENAME" "$HOME/.$FILENAME"; done
 
 
