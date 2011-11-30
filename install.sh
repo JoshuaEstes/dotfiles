924dd6ffd3e16173702202933494344cc9e89d0a
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-11-30 12:06:28 -0600

    added apache2 aliases

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for FILENAME in $( ls | grep -v README | grep -v install.sh | grep -v web_install.sh ); do echo "$FILENAME"; ln -bis "$(pwd)/$FILENAME" "$HOME/.$FILENAME"; done
+for FILENAME in $( ls | grep -v README | grep -v install.sh | grep -v web_install.sh ); do echo "installing $FILENAME"; ln -bis "$(pwd)/$FILENAME" "$HOME/.$FILENAME"; done
 
 
