982c43ab3390074af750a95b94e8cc0e2fdd8744
Joshua Estes <Joshua@dSpaceLabs.com>
2014-11-27 22:12:25 -0500

    email related updates

--- offlineimap.py
+++ offlineimap.py
@@ -11,10 +11,15 @@ import subprocess
 password_storage_path = '~/.pw'
 
 def fetch_password(account, server, algo='ripemd160'):
+    """
+    Takes a hash of the account and server and uses that as the filename
+    for which the password is contained within. The file must already be
+    encrypted using GPG
+    """
     h = hashlib.new(algo)
     h.update(bytearray(account + server, 'utf-8'))
     filename = os.path.expanduser(password_storage_path) + '/' + h.hexdigest() + '.gpg'
-    command = 'gpg --use-agent --quiet --batch -d ' + filename
+    command = 'gpg --no-verbose --output - --use-agent --quiet --batch -d ' + filename
     output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).strip()
 
     return output.decode('utf-8')
