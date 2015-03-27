1355c1bb371293049af9062959431a0a95832cde
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2015-03-27 13:24:54 -0400

    added space

--- bash.d/base.bash
+++ bash.d/base.bash
@@ -12,7 +12,7 @@ esac
 PS1="${TITLE_BAR}\033[1;37m\u@\h \033[0;33m\w "
 
 if [[ -n "$PHPBREW_SET_PROMPT" && "$PHPBREW_SET_PROMPT" == "1" ]]; then
-    PS1=$PS1"\033[0;32m[\033[0;34m\$(phpbrew_current_php_version)\033[0;32m]"
+    PS1=$PS1"\033[0;32m[\033[0;34m\$(phpbrew_current_php_version)\033[0;32m] "
 fi
 
 PS1=$PS1"\033[0;35m\$(__git_ps1 "%s")\033[1;37m\n"
