528358568b8a19e87ce533f157a6e15482bbd071
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2015-11-11 13:42:19 -0500

    updated so it displays no vcs when not in repository

--- lib/shell/plugins/vcs/vcs.plugin.zsh
+++ lib/shell/plugins/vcs/vcs.plugin.zsh
@@ -12,11 +12,15 @@ zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' check-for-staged-changes true
 zstyle ':vcs_info:git+*:*' debug false
 zstyle ':vcs_info:git:*' get-revision true
-zstyle ':vcs_info:git:*' formats '%b (%7.7i)%u%c'
+zstyle ':vcs_info:git:*' formats '%b (%7.7i)%u%c%m'
 zstyle ':vcs_info:git+set-message:*' hooks git-untracked
+zstyle ':vcs_info:*+no-vcs:*' hooks no-vcs-detected
 +vi-git-untracked() {
     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
         git status --porcelain | grep '??' &> /dev/null ; then
         hook_com[staged]+='T'
     fi
+}
++vi-no-vcs-detected() {
+    vcs_info_msg_0_='no vcs'
 }
