ba859587ae24373fd4eb7ed7ecfd2e758b237a23
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2015-11-12 11:42:50 -0500

    have a few updates

--- lib/shell/plugins/vcs/vcs.plugin.zsh
+++ lib/shell/plugins/vcs/vcs.plugin.zsh
@@ -12,7 +12,7 @@ zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' check-for-staged-changes true
 zstyle ':vcs_info:git+*:*' debug false
 zstyle ':vcs_info:git:*' get-revision true
-zstyle ':vcs_info:git:*' formats '%b (%7.7i)%u%c'
+zstyle ':vcs_info:git:*' formats '%b (%7.7i) <%u%c>'
 zstyle ':vcs_info:git+set-message:*' hooks git-untracked
 zstyle ':vcs_info:*+no-vcs:*' hooks no-vcs-detected
 +vi-git-untracked() {
