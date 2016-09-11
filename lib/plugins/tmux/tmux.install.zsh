24a6cab6607b571176fb9afbf0ff8e118ceffbb3
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2016-09-11 10:52:44 -0400

    yey

--- lib/plugins/tmux/tmux.install.zsh
+++ lib/plugins/tmux/tmux.install.zsh
@@ -3,6 +3,11 @@
 
 
 # Install TPM
+# @see https://github.com/tmux-plugins/tpm/blob/master/docs/automatic_tpm_installation.md
 git clone https://github.com/tmux-plugins/tpm $DOTFILES_ROOT/tmux/plugins/tpm
+
+# Install TPM plugins
+# @seehttps://github.com/tmux-plugins/tpm/blob/master/docs/managing_plugins_via_cmd_line.md
+$DOTFILES_ROOT/tmux/plugins/tpm/bin/install_plugins
 
 # Install tmux here
--- lib/plugins/tmux/tmux.update.zsh
+++ lib/plugins/tmux/tmux.update.zsh
@@ -3,11 +3,11 @@
 
 #exit
 
-cd $DOTFILES_ROOT
-git fetch tpm master
-git subtree pull --prefix tmux/plugins/tpm tpm master --squash
+#cd $DOTFILES_ROOT
+#git fetch tpm master
+#git subtree pull --prefix tmux/plugins/tpm tpm master --squash
 # Update all tmux plugins
-$HOME/.tmux/plugins/tpm/bin/update_plugins all
+$DOTFILES_ROOT/tmux/plugins/tpm/bin/update_plugins all
 
 # Remove plugins not on the plugin list
-$HOME/.tmux/plugins/tpm/bin/clean_plugins
+$DOTFILES_ROOT/tmux/plugins/tpm/bin/clean_plugins
