2b3d86bda72969835f0caf7f8b91a157465445b9
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-19 09:29:14 -0400

    comments

--- lib/plugins/phpbrew/phpbrew.install.zsh
+++ lib/plugins/phpbrew/phpbrew.install.zsh
@@ -2,6 +2,7 @@
 
 # Requirements
 # https://github.com/phpbrew/phpbrew/wiki/Requirement
+echo '>>> Installing PHPBrew Requirements'
 xcode-select --install
 brew install automake autoconf curl pcre bison re2c mhash libtool icu4c gettext jpeg openssl libxml2 mcrypt gmp libevent
 brew link icu4c
@@ -9,6 +10,7 @@ brew link --force openssl
 brew link --force libxml2
 
 # Install
+echo '>>> Installing PHPBrew'
 # https://github.com/phpbrew/phpbrew
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod +x phpbrew
@@ -17,3 +19,7 @@ mv phpbrew ~/bin/phpbrew
 # https://github.com/phpbrew/phpbrew/wiki/Quick-Start
 phpbrew init
 phpbrew update
+
+echo '>>>'
+echo 'PHPBrew has been installed, however you still need to install a php version'
+echo '>>>'
